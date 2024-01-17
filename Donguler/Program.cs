3fb1e67f8fcfbe884174005972d51de60e6f2dda
Berkay Kaya <berkaykaya479@gmail.com>
2024-01-17 03:20:41 +0300

    17 Ocak 2024

---
+++ Donguler/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Donguler;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string kurs1 = "Yazılım geliştirici kampı";
+        string kurs2 = "Programlamaya başlangıç kursu";
+        string kurs3 = "Java";
+
+        //Array - diziler
+
+        string[] kurslar = new string[] {"Yazılım geliştirici kampı", "Programlamaya başlangıç kursu", "Java"};
+        for (int i = 0; i < kurslar.Length; i++)
+        {
+            Console.WriteLine(kurslar[i]);
+        }
+        Console.WriteLine("For bitti");
+
+        foreach (string kurs in kurslar)
+        {
+            Console.WriteLine(kurs);
+        }
+
+        Console.WriteLine("Sayfa Sonu - Footer");
+    }
+}
+
--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -1,12 +1,40 @@
 ﻿namespace KampIntro;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        //eğitime başlayacağım
-        //selam dostlar
-        Console.WriteLine("Merhaba arkadaşlar");
+        //type safety - tip güvenliği
+        //Do not repeat yourself.
+        //değer tutucu - alias 
+        string kategoriEtiketi = "Kategori";
+        int ogrenciSayisi = 32000;
+        double faizOrani = 1.45;
+        bool sistemeGirisYapmisMi = true;
+        double dolarDun = 7.35;
+        double dolarBugun = 7.45;
+
+        if (dolarDun > dolarBugun)
+        {
+            Console.WriteLine("Azalış Oku!");
+        }
+        else if (dolarDun < dolarBugun)
+        {
+            Console.WriteLine("Artış Oku!");
+        }
+        else
+        {
+            Console.WriteLine("Değişmedi");
+        }
+
+        if (sistemeGirisYapmisMi == true)
+        {
+            Console.WriteLine("Ayarlar Butonu");
+        }
+        else
+        {
+            Console.WriteLine("Giriş Yap Butonu");
+        }
+        Console.WriteLine(kategoriEtiketi);
     }
 }
-
